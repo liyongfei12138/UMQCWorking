c538475a6e659e312da3bd31d89fc38e718bcd1b
lixin <lixin@uqian.com>
2019-03-19 12:05:39 +0800

    first init

--- UMQCWorking.podspec
+++ UMQCWorking.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'UMQCWorking'
-  s.version          = '1.0.2'
+  s.version          = '1.0.4'
   s.summary          = 'just a demo'
 
 
---
+++ UMQCWorking/Classes/QCKJLibNetTool.h
@@ -0,0 +1,14 @@
+
+
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
+
+typedef void(^SuccessBlock)(id responsData);
+typedef void(^FaileBlock)(NSError *error);
+
+@interface QCKJLibNetTool : NSObject
+@property (nonatomic,strong)AFHTTPSessionManager *manager;
++(instancetype) sharedInstance;
+-(void)oneGet:(NSString*)url success:(SuccessBlock)successBlock faile:(FaileBlock)faileBlock;
+-(void)onePost:(NSString*)url parameters:(id)parameters success:(SuccessBlock)successBlock faile:(FaileBlock)faileBlock ;
+@end
